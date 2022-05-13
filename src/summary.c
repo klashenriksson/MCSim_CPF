341b127301fd6e2be548e5be242c59b32dbb5d6e
klashenriksson <6956114+klashenriksson@users.noreply.github.com>
2022-05-14 00:16:26 +0200

    fix variance bug

--- src/summary.c
+++ src/summary.c
@@ -11,6 +11,7 @@ typedef struct datafile
   double avg_e;
   double avg_c;
   double avg_m;
+  double avg_block_m2;
   double avg_m2;
   double avg_m4;
   double* correlations;
@@ -30,6 +31,7 @@ datafile_t read_file(FILE* f, Par* par)
     datafile.avg_e += r.e;
     datafile.avg_c += r.c;
     datafile.avg_m += r.m;
+    datafile.avg_block_m2 += r.m*r.m;
     datafile.avg_m2 += r.m2;
     datafile.avg_m4 += r.m4;
     datafile.nblocks += 1;
@@ -38,6 +40,7 @@ datafile_t read_file(FILE* f, Par* par)
   datafile.avg_e /= datafile.nblocks;
   datafile.avg_c /= datafile.nblocks;
   datafile.avg_m /= datafile.nblocks;
+  datafile.avg_block_m2 /= datafile.nblocks;
   datafile.avg_m2 /= datafile.nblocks;
   datafile.avg_m4 /= datafile.nblocks;
   datafile.correlations = correlations;
@@ -49,7 +52,7 @@ datafile_t read_file(FILE* f, Par* par)
 // magnetization, error bars for magnetization, and Binder's cumulant.
 void result(Par *par, datafile_t* df)
 {
-  double m_variance = 1.0/(fmax(df->nblocks-1,1)) * (df->avg_m2 - df->avg_m*df->avg_m);
+  double m_variance = 1.0/(fmax(df->nblocks-1,1)) * (df->avg_block_m2 - df->avg_m*df->avg_m);
   double Q = df->avg_m2*df->avg_m2/df->avg_m4;
   printf("L %d T %8f m %8f m_variance %8f Q %8f\n", par->L, par->t, df->avg_m, m_variance, Q);
 }
