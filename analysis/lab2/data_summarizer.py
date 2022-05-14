4044e2f4b8761c4ab6c8bca3c009bfaa5c8bfe3b
klashenriksson <6956114+klashenriksson@users.noreply.github.com>
2022-05-14 11:16:36 +0200

    regenerated data making sure it was correct

--- analysis/lab2/data_summarizer.py
+++ analysis/lab2/data_summarizer.py
@@ -6,12 +6,16 @@ import platform
 def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
-args = sys.argv[1:]
+if len(sys.argv) < 3:
+    raise ValueError("Need at least 2 args specifying dir and file search text")
+
+args = sys.argv[2:]
+root = sys.argv[1]
 files = {arg: [] for arg in args}
 summary_filepath = os.path.join(get_script_path(), "summary.exe" if platform.system() == "Windows" else "summary")
 
-if not os.path.isdir(os.path.join(get_script_path(), "data_summary")):
-    os.mkdir(os.path.join(get_script_path(), "data_summary"))
+if not os.path.isdir(os.path.join(get_script_path(), root)):
+    os.mkdir(os.path.join(get_script_path(), root))
 
 for filepath in os.listdir("data/"):
     for arg in args:
@@ -20,7 +24,7 @@ for filepath in os.listdir("data/"):
             files[arg].append(fullpath)
 
 for arg in files.keys():
-    with open(os.path.join("data_summary", f"{arg}.txt"), "w+") as f:
+    with open(os.path.join(root, f"{arg}.txt"), "w+") as f:
         process = subprocess.Popen([summary_filepath], text=True, stdin=subprocess.PIPE, stdout=f, stderr=subprocess.PIPE, cwd=get_script_path())
         input = '\n'.join([str(path) + "\n" for path in files[arg]])
         process.communicate(input=input)
